0147039987677b05b62e197df9eb3a260c8f0e55
Hossein <hoseinshahmohammady@gmail.com>
2025-08-21 14:02:44 -0700

    add config/urls

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from textto3d.views import text_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', text_view, name='search_model'),
 ]
